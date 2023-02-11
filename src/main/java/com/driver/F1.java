cc504d7052e8066fa80fe6a5d41e8fda9faeec51
unknown <verma.shubham2537@gmail.com>
2023-02-11 17:47:59 +0530

    First Commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 5, 6, isManual, "SUV", 4);
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed = getCurrentSpeed() + rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,8 +22,40 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            super.stop();
+            setCurrentGear(1);
         }
         //for all other cases, change the gear accordingly
+        else if(newSpeed > 0 && newSpeed <= 50) {
+            //Stop the car, set gear as 1
+            setCurrentGear(1);
+            setCurrentSpeed(newSpeed);
+        }
+        else if(newSpeed > 50 && newSpeed <= 100) {
+            //Stop the car, set gear as 1
+            setCurrentGear(2);
+            setCurrentSpeed(newSpeed);
+        }
+        else if(newSpeed > 100 && newSpeed <= 150) {
+            //Stop the car, set gear as 1
+            setCurrentGear(3);
+            setCurrentSpeed(newSpeed);
+        }
+        else if(newSpeed > 150 && newSpeed <= 200) {
+            //Stop the car, set gear as 1
+            setCurrentGear(4);
+            setCurrentSpeed(newSpeed);
+        }
+        else if(newSpeed > 200 && newSpeed <= 250) {
+            //Stop the car, set gear as 1
+            setCurrentGear(5);
+            setCurrentSpeed(newSpeed);
+        }
+        else if(newSpeed > 250) {
+            //Stop the car, set gear as 1
+            setCurrentGear(6);
+            setCurrentSpeed(newSpeed);
+        }
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
